0e7dfc3d9e07c091cde0327de8c21d2bbfec246e
EnzoLsena <enzolevysena@gmail.com>
2025-06-01 19:47:59 -0300

    feat: add card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,31 @@
 <script setup lang="ts">
 import Button from 'primevue/button'
+import Card from 'primevue/card'
 import { toast, Toaster } from "vue-sonner"
 import { ref, onMounted } from 'vue';
 import Weather from "@/service-weather"
 
 const cidade = ref('Aracaju, SE');
-const city =ref("Paris")
-const citys= ref([]);
+const city = ref("Paris")
+const places = ref({});
 const success = () => {
  return toast.success('Brisio é gay')
 }
 
 const getWeatherData =() => {
     Weather.getTemperature(city.value)
-    .then(({data}) => {
-        const pais = data.map((elem: any) => {
-            return {
-                cityName: elem.name,
-                countryName: elem.sys.country 
-            }
-        });
-        citys.value = pais; 
+        .then(({ data }) => {
+            places.value = {
+                name: data.name,
+                temp: data.main.temp,
+                humidity: data.main.humidity,
+                pressure: data.main.pressure,
+        }
+        
     }) 
     .finally(()=>{
-
+        cidade.value = `${places.value.name}, ${places.value.temp}°C`
+        console.log(places.value)
     })
 }
 
@@ -34,7 +36,7 @@ onMounted(()=>{
 
 <template>
 <main class="w-full h-full bg-secondary">
-    <Card class="container mx-auto p-4">
+    <Card class=" h-[20rem] w-[20rem] mx-auto p-4">
         <h1 class="text-3xl font-bold text-center mb-6 text-black">Temperatura Atual</h1>
         <p class="text-lg text-center">{{ cidade }}</p>
         <Toaster position="top-right" richColors/>
